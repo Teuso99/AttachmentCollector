85a8dacc49369c33109f48ede2939c97656a3c2b
teuso <mateusmmachado2@gmail.com>
2025-12-22 18:57:22 -0300

    WIP refactor

--- AttachmentCollector.ConsoleApp/Program.cs
+++ AttachmentCollector.ConsoleApp/Program.cs
@@ -1,19 +1,26 @@
 ﻿using AttachmentCollector.ConsoleApp;
 using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Responses;
 using Google.Apis.Drive.v3;
 using Google.Apis.Gmail.v1;
-
-if (args[0] is null)
-{
-    throw new ApplicationException("Enter the secret's file name");
-}
-
-var secretFile = Environment.CurrentDirectory + "\\" + args[0];
 
 const string userId = "me";
 string[] scopes = [DriveService.Scope.Drive, GmailService.Scope.GmailModify];
 
-var credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(GoogleClientSecrets.FromFile(secretFile).Secrets, scopes, "user", CancellationToken.None);
+var credential = new UserCredential(
+    new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+    {
+        ClientSecrets = new ClientSecrets
+        {
+            ClientId = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_ID"),
+            ClientSecret = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_SECRET")
+        },
+        Scopes = scopes
+    }),
+    userId,
+    new TokenResponse { RefreshToken = Environment.GetEnvironmentVariable("GOOGLE_REFRESH_TOKEN") }
+);
 
 if (credential.Token.IsStale)
 {
